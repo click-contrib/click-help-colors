b5c8eba708c730307c53231c71479b65338b1282
rmn <r.y.t@yandex.ru>
2016-09-01 00:12:17 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='click_help_colors',
+    version=0.1,
+    packages=['click_help_colors'],
+    install_requires=[
+        'click'
+    ]
+)
