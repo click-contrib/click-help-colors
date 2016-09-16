1eb32dcc357653e2205a73fdf67343571b543e21
rmn <r.y.t@yandex.ru>
2016-09-16 10:34:21 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='click-help-colors',
-    version=0.2,
+    version=0.3,
     packages=['click_help_colors'],
     description='Colorization of help messages in Click',
     url='https://github.com/r-m-n/click-help-colors',
